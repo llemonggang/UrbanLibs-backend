e40275cce31207d4c82188bef6b09834e42711c7
Richard Davis <rddavistx@hotmail.com>
2016-09-20 12:24:27 -0500

    random word

--- models/word.js
+++ models/word.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+var random = require('mongoose-random');
+
 
 var wordSchema = new mongoose.Schema({
   word: String,
   type: String
 })
+
+wordSchema.plugin(random, { path: 'r' });
 
 const Word = mongoose.model('Word', wordSchema);
 
