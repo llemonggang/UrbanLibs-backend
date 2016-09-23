41f94a5aa3d55f6a28264310654667d4e07ffee8
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-09-23 12:36:30 -0500

    change api route

--- routes/words.js
+++ routes/words.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Word = require('../models/word.js')
 var random = require('mongoose-random');
+var http = require('http');
 
 
 router.get('/random', function(req, res, next) {
@@ -20,6 +21,29 @@ router.get('/random', function(req, res, next) {
     })
   })
 })
+
+router.get('/define/:word', function(req, res, next) {
+  var wordToDefine = req.params.word;
+
+  // http://api.urbandictionary.com/v0/define?term= + wordToDefine
+
+  http.get({
+        host: 'api.urbandictionary.com',
+        path: '/v0/define?term=' + wordToDefine
+    }, function(response) {
+        var body = '';
+
+        response.on('data', function(d) {
+            body += d;
+        });
+
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            res.json(parsed)
+        });
+    });
+})
+
 
 // router.get('/sync', function(req, res, next) {
 //   Word.syncRandom(function (err, result) {
